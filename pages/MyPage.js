9e6df4dd28c14bcc6b2a9027c5524e37c155283c
kaori <kaori@weseek.co.jp>
2021-01-10 22:26:43 +0900

    create mypage just for an example

---
+++ pages/MyPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MyPage = () => {
+    return (
+        <h1>This is my page</h1>
+    )
+}
+
+export default MyPage;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import MyPage from './MyPage';
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Hi
         </h1>
+        <MyPage />
 
         <div className={styles.grid}>
         </div>
